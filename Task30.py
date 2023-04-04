e11ccbcc15081c841a866fd935895c36ffae299e
Valentin.Kostin <frezer.2021@mail.ru>
2023-04-04 08:13:57 +0300

    task 30 gotovo

---
+++ Task30.py
@@ -0,0 +1,13 @@
+
+def progressiv_array(num1, step, size):
+    arr = []
+    for i in range(n):
+        arr.append(a1 + i * d)
+    return arr
+
+
+a1 = int(input('введите число начала ряда: '))
+d = int(input('введите число шага ряда: '))
+n = int(input('введите длину ряда: '))
+
+print(progressiv_array(a1, d, n))
