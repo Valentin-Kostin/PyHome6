d17de6355fa94c4bdd1777d1211095fe63223902
Valentin.Kostin <frezer.2021@mail.ru>
2023-04-04 08:20:38 +0300

    task 32 gotovo

--- Task32.py
+++ Task32.py
@@ -3,8 +3,8 @@
 # (т.е. не меньше заданного минимума и не больше заданного максимума)
 
 list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
-min_number = int(input())
-max_number = int(input())
+min_number = int(input('Введите минимальное число: '))
+max_number = int(input('Введите максимальное число: '))
 for i in range(len(list_1)):
     if min_number <= list_1[i] <= max_number:
         print(i)
